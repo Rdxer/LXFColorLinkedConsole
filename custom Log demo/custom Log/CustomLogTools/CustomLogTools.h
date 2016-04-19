42447776dfdc8862744ab5b2b491eec823867833
lxf <rdxer@foxmail.com>
2016-04-19 22:36:58 +0800

    ..

--- custom Log demo/custom Log/CustomLogTools/CustomLogTools.h
+++ custom Log demo/custom Log/CustomLogTools/CustomLogTools.h
@@ -12,9 +12,9 @@
 
 #define Xassert(condition, desc, ...) {\
 if((condition) == NO){\
-NSString *str = [NSString stringWithFormat:desc,##__VA_ARGS__];    \
-LXFPrintf("💔",@"断言失败 -> %@", str);\
-NSAssert(NO, str);\
+    NSString *str = [NSString stringWithFormat:desc,##__VA_ARGS__];    \
+    LXFPrintf("💔",@"断言失败 -> %@", str);\
+    NSAssert(NO, str);\
 }\
 }
 
@@ -24,53 +24,53 @@ NSAssert(NO, str);\
 
 
 
-#define NSLog(format, ...)   {\
-LXFPrintf("😐",format, ##__VA_ARGS__); \
-}
-// error
-#define printE(format, ...)   {\
-LXFPrintf("❌",format, ##__VA_ARGS__); \
-}
-// debug
-#define printD(format, ...)   {\
-LXFPrintf("😬",format, ##__VA_ARGS__); \
-}
-// print obj
-#define printOBJ(obj)   {\
-LXFPrintf("⚽️",@"%@",obj); \
-}
-// wran
-#define printW(format, ...)   {\
-LXFPrintf("⚠️",format, ##__VA_ARGS__); \
-}
-#define LXFPrintf(tag,format, ...)  printf("%s:%d %s%s + %d🎈 %s\n",[NSString stringWithUTF8String:__FILE__].lastPathComponent.UTF8String,__LINE__,\
-tag, __PRETTY_FUNCTION__, __LINE__,\
-[[NSString stringWithFormat:format,\
-##__VA_ARGS__]UTF8String])
+    #define NSLog(format, ...)   {\
+        LXFPrintf("😐",format, ##__VA_ARGS__); \
+        }
+    // error
+    #define printE(format, ...)   {\
+        LXFPrintf("❌",format, ##__VA_ARGS__); \
+        }
+    // debug
+    #define printD(format, ...)   {\
+        LXFPrintf("😬",format, ##__VA_ARGS__); \
+        }
+    // print obj
+    #define printOBJ(obj)   {\
+        LXFPrintf("⚽️",@"%@",obj); \
+    }
+    // wran
+    #define printW(format, ...)   {\
+        LXFPrintf("⚠️",format, ##__VA_ARGS__); \
+        }
+    #define LXFPrintf(tag,format, ...)  printf("%s:%d %s%s + %d🎈 %s\n",[NSString stringWithUTF8String:__FILE__].lastPathComponent.UTF8String,__LINE__,\
+    tag, __PRETTY_FUNCTION__, __LINE__,\
+    [[NSString stringWithFormat:format,\
+    ##__VA_ARGS__]UTF8String])
 
 #else
 
-// print obj
-#define printOBJ(obj)   {\
-LXFPrintf("⚽️",@"%@",obj); \
-}
+    // print obj
+    #define printOBJ(obj)   {\
+        LXFPrintf("⚽️",@"%@",obj); \
+    }
 
-#define NSLog(format, ...)
-// error
-#define printE(format, ...)   {\
-LXFPrintf("❌",format, ##__VA_ARGS__); \
-}
+    #define NSLog(format, ...)
+    // error
+    #define printE(format, ...)   {\
+        LXFPrintf("❌",format, ##__VA_ARGS__); \
+    }
 
-// debug
-#define printD(format, ...)
+    // debug
+    #define printD(format, ...)
 
-// wran
-#define printW(format, ...)
+    // wran
+    #define printW(format, ...)
 
-#define LXFPrintf(tag,format, ...)  printf("%s:%d %s%s + %d🎈 %s\n",[NSString stringWithUTF8String:__FILE__].lastPathComponent.UTF8String,__LINE__,\
-tag, __PRETTY_FUNCTION__, __LINE__,\
-[[NSString stringWithFormat:format,\
-##__VA_ARGS__]UTF8String])
+    #define LXFPrintf(tag,format, ...)  printf("%s:%d %s%s + %d🎈 %s\n",[NSString stringWithUTF8String:__FILE__].lastPathComponent.UTF8String,__LINE__,\
+    tag, __PRETTY_FUNCTION__, __LINE__,\
+    [[NSString stringWithFormat:format,\
+    ##__VA_ARGS__]UTF8String])
 
 #endif
 
